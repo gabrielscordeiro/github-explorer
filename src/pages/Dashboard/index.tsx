c7035b871411a42785a616f9d8b68179adf9d644
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2022-03-08 22:55:36 -0300

    feat: 🎸 Conectand na API do GitHub e listando repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,68 +1,70 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import { Title, Form, Repositories } from './styles';
 
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import api from '../../services/api';
 /*
 React.FC = React Function Component
 */
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logoImg} alt="GitHub Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/8952285?v=4"
-                        alt="Gabriel Schmidt Cordeiro"
-                    />
-                    <div>
-                        <strong>gabrielscordeiro/gobarber-api</strong>
-                        <p>
-                            API desenvolvida para o o Projeto GoBarber do
-                            Bootcamp GoStack da Rocketseat
-                        </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/8952285?v=4"
-                        alt="Gabriel Schmidt Cordeiro"
-                    />
-                    <div>
-                        <strong>gabrielscordeiro/gobarber-api</strong>
-                        <p>
-                            API desenvolvida para o o Projeto GoBarber do
-                            Bootcamp GoStack da Rocketseat
-                        </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/8952285?v=4"
-                        alt="Gabriel Schmidt Cordeiro"
-                    />
-                    <div>
-                        <strong>gabrielscordeiro/gobarber-api</strong>
-                        <p>
-                            API desenvolvida para o o Projeto GoBarber do
-                            Bootcamp GoStack da Rocketseat
-                        </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
+                {repositories.map((repository) => (
+                    <a key={repository.full_name} href="teste">
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Repositories>
         </>
     );
