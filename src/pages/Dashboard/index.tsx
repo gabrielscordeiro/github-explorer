0d77fdca3723bd4a82ab56d4f66f81e7819be3fb
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2022-03-08 22:15:42 -0300

    style: 💄 Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,71 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
 /*
 React.FC = React Function Component
 */
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no GitHub</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Explore repositórios no GitHub</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/8952285?v=4"
+                        alt="Gabriel Schmidt Cordeiro"
+                    />
+                    <div>
+                        <strong>gabrielscordeiro/gobarber-api</strong>
+                        <p>
+                            API desenvolvida para o o Projeto GoBarber do
+                            Bootcamp GoStack da Rocketseat
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/8952285?v=4"
+                        alt="Gabriel Schmidt Cordeiro"
+                    />
+                    <div>
+                        <strong>gabrielscordeiro/gobarber-api</strong>
+                        <p>
+                            API desenvolvida para o o Projeto GoBarber do
+                            Bootcamp GoStack da Rocketseat
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/8952285?v=4"
+                        alt="Gabriel Schmidt Cordeiro"
+                    />
+                    <div>
+                        <strong>gabrielscordeiro/gobarber-api</strong>
+                        <p>
+                            API desenvolvida para o o Projeto GoBarber do
+                            Bootcamp GoStack da Rocketseat
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,94 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
     font-size: 48px;
     color: #3a3a3a;
+    max-width: 450px;
+    line-height: 56px;
+    margin-top: 80px ;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    margin-top: 40px;
+    max-width: 715px;
+
+    input{
+        flex: 1;
+        height: 70px;
+        padding: 0 24px;
+        border: 0;
+        border-radius: 5px 0 0 5px;
+        color: #3a3a3a;
+
+        &::placeholder{
+            color: #a8a8b3
+        }
+    }
+
+    button{
+        width: 210px;
+        height: 70px;
+        background: #04D361;
+        border-radius: 0px 5px 5px 0px;
+        border: 0;
+        color: #fff;
+        font-weight: 500;
+        transition: background-color 0.2s ;
+
+        &:hover{
+            background: ${shade(0.2, '#04D361')};
+        }
+    }
+`;
+
+export const Repositories = styled.div`
+    max-width: 715px;
+    margin-top: 80px;
+
+    a{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        background: #fff;
+        border-radius: 5px;
+        padding: 24px;
+        text-decoration:  none;
+        transition: transform 0.2s;
+
+        & + a{
+            margin-top: 16px;
+        }
+
+        &:hover{
+            transform: translateX(10px);
+        }
+
+        img{
+            width: 54px;
+            height: 54px;
+            border-radius: 50%;
+        }
+
+        div{
+            margin-left: 16px;
+
+            strong{
+                font-size: 20px;
+                color: #3d3d4d;
+            }
+
+            p{
+                font-size: 18px;
+                color: #a8a8b3;
+                margin-top: 4px;
+            }
+        }
+
+        svg{
+            margin-left: auto;
+            color: #C9C9D4;
+        }
+    }
 `
